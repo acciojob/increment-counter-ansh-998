efefa746068d5bafd0766a8805da59e6d4c4c12e
Ansh Gangwar <anshgangwar998@gmail.com>
2025-11-14 21:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let count = 0;
+
+document.getElementById("incrementBtn").addEventListener("click", function () {
+  alert(count);
+  count++;
+  document.getElementById("counter").innerText = count;
+});
